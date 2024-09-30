428aef4fbb28c764a9776af672135f3e612fa642
AbinSibi466 <abin.sibi@mca.christuniversity.in>
2024-09-30 17:34:20 +0530

    double checking every thing

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import Login from '../components/User/Login/Login'
 import Register from '../components/User/Register/Register'
 import './LoginPage.css'
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
     const [login,setLogin] = useState(true);
     const [register,setRegister] = useState(false);
+    const navigate = useNavigate();
 
     const showLogin = ()=>{
         setLogin(true);
@@ -15,6 +17,10 @@ function LoginPage() {
         setRegister(true)
         setLogin(false)
     }
+    const adminLogin = ()=>{
+      navigate('/admin');
+  }
+
   return (
     <div className='loginPage'>
         <div className='loginPageBox'>
@@ -24,6 +30,7 @@ function LoginPage() {
 {register &&<button onClick={showLogin} className='lbuttons' >Already a user ? - login</button>}
 
 {login && <button onClick={showRegister} className='lbuttons'>New user ? - register</button>}
+{login && <button onClick={adminLogin} className='labuttons'>login as Admin</button>}
         </div>
 
 
